d185092268579c1a0560b58620dd6bea1346f7b6
JoeIt <josignaciot@gmail.com>
2022-11-08 14:40:56 -0300

    Dependencies installed

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,13 @@
 const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
+module.exports = sequelize => {
   // defino el modelo
   sequelize.define('country', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      primaryKey: true,
     },
   });
 };
