dddd093d662610544f992f050d52aba29fc6d6ff
JoeIt <josignaciot@gmail.com>
2022-11-13 23:16:34 -0300

    react router part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import { MainPage, Home, Country, Form, PageNotFound } from './components';
+import Navbar from './components/ui/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/countries">
+          <Navbar />
+          <MainPage />
+        </Route>
+        <Route exact path="/form">
+          <Navbar />
+          <Form />
+        </Route>
+        <Route exact path="/countries/:name">
+          <Navbar />
+          <Country />
+        </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import Home from './Home';
+import MainPage from './MainPage';
+import Form from './Form';
+import Country from './Country';
+import PageNotFound from './PageNotFound';
+
+export { Home, MainPage, Form, Country, PageNotFound };
